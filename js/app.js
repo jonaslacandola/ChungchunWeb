8926b4bcf09d2aaf8e2e9678e81c943b841cb445
jlaca <jonaslacandola07@gmail.com>
2023-07-01 10:17:13 +0800

    some changes

--- js/app.js
+++ js/app.js
@@ -78,6 +78,12 @@ const addProduct = (product, price, description, image_url, parent, subtitle) =>
     const card_subtitle = document.createElement('div')
     
     card_img.src = image_url
+    if (card_img.src == null) {
+        card_img.classList.add('placeholder')
+    } else if (card_img.src != null && card_img.classList.contains('placeholder')) {
+        card_img.classList.remove('placeholder')
+    }
+
     card_title.innerText = product
     card_text.innerText = description
     card_footer_text.innerText = 'P ' + price
